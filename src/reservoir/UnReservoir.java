c295b6c5d99d6b7ad6d3244f04cc607d7ecb2e40
fcd <fcd@WIN-O4SV84MG8NT>
2016-04-04 16:18:17 +0200

    creation de la classe pour gérer les resevoir avec ces méthodes

--- src/reservoir/UnReservoir.java
+++ src/reservoir/UnReservoir.java
@@ -17,16 +17,27 @@ public class UnReservoir {
     
     public UnReservoir(int p_litreContenue){
         litreContenue = p_litreContenue;
-        if(p_litreContenue > 0){
-            vide = false;
-        }
-        else if(p_litreContenue == 100){
-            vide = false;
-            plein = true;
-        }
+        testPleinVide();
         contenuMax = 100;
     }
     
+    private void testPleinVide(){
+        if(litreContenue > 0){
+            vide = false;
+        }
+        else if(litreContenue == 100){
+            vide = false;
+            plein = true;
+        }
+    }
+    
+    public boolean estVide(){
+        return vide;
+    }
+    
+    public boolean estPlein(){
+        return plein;
+    }
     public int getCapacite(){
         return contenuMax;
     }
@@ -59,17 +70,29 @@ public class UnReservoir {
         if(((100 -reservoirRemplir) > reservoirVider)){
             p_reservoirRemplir.setNiveau(reservoirVider);
             litreContenue = 0;
+            testPleinVide();
         }
     }
      public void remplir(UnReservoir p_reservoirRemplir){
         int reservoirVider = this.litreContenue;
         int reservoirRemplir = p_reservoirRemplir.getNiveau();
-        if(reservoirVider >= (100-reservoirRemplir)){
+        if(/*reservoirVider >= (100-reservoirRemplir) && */(reservoirRemplir + reservoirVider) >= 100){
             int difference = reservoirVider - (100 - reservoirRemplir);
             reservoirRemplir += (reservoirVider -difference);
             reservoirVider -= (reservoirVider -difference);
+            testPleinVide();
         }
      }
     
+     public void mettreUnDans(UnReservoir p_reservoirRemplir){
+         int reservoirVider = this.litreContenue;
+        int reservoirRemplir = p_reservoirRemplir.getNiveau();
+        if(reservoirVider != 0 && reservoirRemplir != contenuMax){
+            this.litreContenue--;
+            //p_reservoirRemplir.setNiveau(p_reservoirRemplir.getNiveau()+1);
+            p_reservoirRemplir.litreContenue++;
+            testPleinVide();
+        }
+     }
     
 }
